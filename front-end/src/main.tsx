bf46a83cd7c1775dcf2af3912abc8b3daf086255
Mateusz <m.wesolowski.92@gmail.com>
2025-04-06 22:35:47 +0200

    Add favourites path to routes

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,11 +5,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainPage } from "./views/MainPage/MainPage.tsx";
+import { Favourites } from "./views/Favourites/Favourites.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
+  },
+  {
+    path: "/ulubione",
+    element: <Favourites />,
   },
 ]);
 

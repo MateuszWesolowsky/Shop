7673207e4e088582c7a5602b641af6dbda3786c3
Mateusz <m.wesolowski.92@gmail.com>
2025-04-06 22:32:07 +0200

    Create types

---
+++ front-end/src/types/types.ts
@@ -0,0 +1,13 @@
+export interface ProductTypes {
+  id: number;
+  gender: string;
+  category: string;
+  subcategory: string;
+  productName: string;
+  brand: string;
+  pricePLN: number;
+  priceUSD: number;
+  photos: string[];
+  description: string;
+  maintenanceInfo: string;
+}

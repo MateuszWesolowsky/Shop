696e2b9d480bafc00715ec239e613ff42e4d4e17
Mateusz <m.wesolowski.92@gmail.com>
2025-04-22 15:01:43 +0200

    Update paths and add loaders

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,25 +1,40 @@
 import "./styles/theme.css";
 import "./styles/globa.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainPage } from "./views/MainPage/MainPage.tsx";
 import { Favourites } from "./views/Favourites/Favourites.tsx";
 import { Cart } from "./views/Cart/Cart.tsx";
+import { Layout } from "./components/Layout/Layout.tsx";
+import { mainPageLoader } from "./api/mainPageLoader.ts";
+import { ProductsList } from "./views/ProductsList/ProductsList.tsx";
+import { productListLoader } from "./api/productListLoader.ts";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <MainPage />,
-  },
-  {
-    path: "/ulubione",
-    element: <Favourites />,
-  },
-  {
-    path: "/koszyk",
-    element: <Cart />,
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "/koszyk",
+        element: <Cart />,
+      },
+      {
+        path: "/ulubione",
+        element: <Favourites />,
+      },
+      {
+        path: "/:gender?",
+        element: <MainPage />,
+        loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
+      },
+    ],
   },
 ]);
 

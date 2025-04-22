e658172d483b65fa71476803eb46555953fccde1
Mateusz <m.wesolowski.92@gmail.com>
2025-04-22 14:59:09 +0200

    Remove static data and implement dynamic

--- front-end/src/components/CategoryMenu/CategoryMenu.tsx
+++ front-end/src/components/CategoryMenu/CategoryMenu.tsx
@@ -1,14 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import styles from "./CategoryMenu.module.css";
 import { CATEGORIES } from "../../constatns/categories";
 
 export const CategoryMenu = () => {
+  const params = useParams();
+
   return (
     <div className={styles.categoryMeny}>
       <ul>
         {CATEGORIES.map((el) => (
           <li key={el.path}>
-            <NavLink to={el.path}>{el.categoryName}</NavLink>
+            <NavLink to={`${params.gender}/${el.path}`}>
+              {el.categoryName}
+            </NavLink>
           </li>
         ))}
       </ul>
--- front-end/src/components/ExpandableMenu/ExpandableMenu.tsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.tsx
@@ -1,19 +1,29 @@
 import styles from "./ExpandableMenu.module.css";
 import { CATEGORIES } from "../../constatns/categories";
 import ARROW_ICON from "../../assets/arrow.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+
+const PATH_TO_GENDER_NAME = {
+  kobieta: "Kobieta",
+  mezczyzna: "Mężczyzna",
+  dziecko: "Dziecko",
+};
 
 export const ExpandableMenu = () => {
-  const activePanth = "odziez";
+  const params = useParams();
+  const activePanth = params.category;
+
+  const genderLabel =
+    PATH_TO_GENDER_NAME[params.gender as keyof typeof PATH_TO_GENDER_NAME];
 
   return (
     <div className={styles.expandableMenu}>
-      <p>Kobieta</p>
+      <p>{genderLabel}</p>
       <ul>
         {CATEGORIES.map((category) => {
           return (
             <li key={category.path}>
-              <NavLink to={category.path}>
+              <NavLink to={`/${params.gender}/${category.path}`}>
                 {category.categoryName}{" "}
                 <img
                   src={ARROW_ICON}
@@ -28,7 +38,9 @@ export const ExpandableMenu = () => {
                   {category.subcategories.map((subcategory) => {
                     return (
                       <li key={subcategory.path}>
-                        <NavLink to={subcategory.path}>
+                        <NavLink
+                          to={`/${params.gender}/${params.category}/${subcategory.path}`}
+                        >
                           {subcategory.categoryName}
                         </NavLink>
                       </li>

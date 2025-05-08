33a6ed5abfe2cd0b14fff001ba18708b63958763
Mateusz <m.wesolowski.92@gmail.com>
2025-05-08 16:51:43 +0200

    Add dispatch fn and Form from useFetcher to use POST action

--- front-end/src/components/Product/Product.tsx
+++ front-end/src/components/Product/Product.tsx
@@ -1,18 +1,42 @@
 import styles from "./Product.module.css";
-import { Link } from "react-router-dom";
+import { Link, useFetcher } from "react-router-dom";
 import { ProductTypes } from "../../types/types";
+import { Price } from "../Price/Price";
 
 interface ProductProps {
   product: ProductTypes;
 }
 
+const ENDPOINT_TO_PATH_MAPPING = {
+  men: "mezczyzna",
+  women: "kobieta",
+  children: "dziecko",
+};
+
 export const Product = ({ product }: ProductProps) => {
+  const { Form } = useFetcher();
+
   return (
-    <Link className={styles.product}>
+    <Link
+      to={`/${ENDPOINT_TO_PATH_MAPPING[product.gender]}/${product.category}/${
+        product.subcategory
+      }/${product.id}`}
+      className={styles.product}
+    >
       <img src={product.photos[0]} alt="Product photo" />
       <h3>{product.productName}</h3>
-      <p>{product.pricePLN} zł</p>
-      <div className={styles.heart}></div>
+      <p>
+        <Price product={product} />
+      </p>
+      <Form
+        onClick={(e) => e.stopPropagation()}
+        method="POST"
+        action={`/add-to-favourites/${product.id}`}
+      >
+        <button>
+          <div className={styles.heart}></div>
+        </button>
+      </Form>
     </Link>
   );
 };

9d3e8ab0f3ec5ee3e25c8c227d26e56d30098d15
Mateusz <m.wesolowski.92@gmail.com>
2025-05-08 16:43:25 +0200

    Add dispatch fn and save currency into localStorage

--- front-end/src/components/CurrencySelector/CurrencySelector.tsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.tsx
@@ -1,9 +1,28 @@
 import styles from "./CurrencySelector.module.css";
 import { CURRENCIES } from "../../constatns/currencies";
+import {} from "react-redux";
+import { useDispatch } from "react-redux";
+import { changeCurrency } from "../../store/currencySlice";
+import { Currency } from "../../types/types";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 export const CurrencySelector = () => {
+  const dispatch = useDispatch();
+  const currency = useSelector(
+    (state: RootState) => state.currency.currencyValue
+  );
+
   return (
-    <select className={styles.currencySelector} name="currency">
+    <select
+      value={currency}
+      onChange={(e) => {
+        dispatch(changeCurrency(e.currentTarget.value as Currency));
+        localStorage["selected_currency"] = e.currentTarget.value;
+      }}
+      className={styles.currencySelector}
+      name="currency"
+    >
       <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
       <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
     </select>

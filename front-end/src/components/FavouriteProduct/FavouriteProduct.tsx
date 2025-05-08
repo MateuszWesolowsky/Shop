620be09664ac493a2cce161772b4a65b0a6b181f
Mateusz <m.wesolowski.92@gmail.com>
2025-05-08 16:47:11 +0200

    Add dispatch fn and Form from useFetcher to use DELETE action

--- front-end/src/components/FavouriteProduct/FavouriteProduct.tsx
+++ front-end/src/components/FavouriteProduct/FavouriteProduct.tsx
@@ -1,13 +1,23 @@
 import styles from "./FavouriteProduct.module.css";
 import DELETE from "../../assets/remove.svg";
 import CART from "../../assets/bag.svg";
-import { ProductTypes } from "../../types/types";
+import { FavouriteProductTypes } from "../../types/types";
+import { useFetcher } from "react-router-dom";
+import { Price } from "../Price/Price";
+import { addProductToCart } from "../../store/cartSlice";
+import { useDispatch } from "react-redux";
 
 interface Props {
-  product: ProductTypes;
+  favourite: FavouriteProductTypes;
 }
 
-export const FavouriteProduct = ({ product }: Props) => {
+export const FavouriteProduct = ({ favourite }: Props) => {
+  const dispatch = useDispatch();
+  const { Form } = useFetcher();
+
+  const product = favourite.product;
+  const price = <Price product={product} />;
+
   return (
     <div className={styles.favouriteProduct}>
       <img src={product.photos[0]} alt="Photo" />
@@ -17,18 +27,23 @@ export const FavouriteProduct = ({ product }: Props) => {
           <h3>
             {product.brand} {product.productName}
           </h3>
-          <p>{product.pricePLN}zł</p>
+          <p>{price}</p>
         </div>
         <p className={styles.priceRow}>
           <span>Cena: </span>
-          {product.pricePLN}zł
+          {price}
         </p>
         <div className={styles.buttonRow}>
-          <button>
-            <img src={DELETE} alt="Delete Icon" />
-            Usuń
-          </button>
-          <button>
+          <Form
+            action={`/delete-from-favourites/${favourite.id}`}
+            method="DELETE"
+          >
+            <button>
+              <img src={DELETE} alt="Delete Icon" />
+              Usuń
+            </button>
+          </Form>
+          <button onClick={() => dispatch(addProductToCart(product))}>
             <img src={CART} alt="Cart Icon" />
             Dodaj do koszyka
           </button>
